ad18dadfebb558416d873938f049d590abec00d9
unknown <bartlomiejzadlo@gmail.com>
2019-02-11 16:23:54 +0100

    Basic implementation of Stack with tests

---
+++ AlgoDs/AlgoDs/DataStructures/Stack.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDs.DataStructures
+{
+    public class Stack
+    {
+        private object[] container;
+        private int size;
+        private int index;
+
+        public Stack(int size)
+        {
+            Container = new object[size];
+            Size = size;
+            Index = 0;
+        }
+
+        public void Push(object element)
+        {
+            if(Index == Size)
+            {
+                throw new StackOverflowException();
+            }
+
+            Container[Index] = element;
+            Index++;
+        }
+
+        public object Pop()
+        {
+            if(Index == 0)
+            {
+                throw new StackUnderflowException();
+            }
+            object objToReturn = Container[Index - 1];
+            Container[Index - 1] = 0;
+            index--;
+            return objToReturn;
+        }
+
+        public object Peek()
+        {
+            return Container[Index - 1];
+        }
+
+        public int SizeRemaining()
+        {
+            return Size - Index;
+        }
+
+        public object[] Container { get => container; set => container = value; }
+        public int Size { get => size; set => size = value; }
+        public int Index { get => index; set => index = value; }
+
+        [Serializable]
+        public class StackUnderflowException : Exception
+        {
+            public StackUnderflowException()
+            {
+            }
+
+            public StackUnderflowException(string message) : base(message)
+            {
+            }
+
+            public StackUnderflowException(string message, Exception innerException) : base(message, innerException)
+            {
+            }
+
+            protected StackUnderflowException(SerializationInfo info, StreamingContext context) : base(info, context)
+            {
+            }
+        }
+    }
+}
---
+++ AlgoDs/AlgoDsTests/DataStructures/StackTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgoDs.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDs.DataStructures.Tests
+{
+    [TestClass()]
+    public class StackTests
+    {
+        private Stack stack;
+
+        [TestInitialize()]
+        public void TestInit()
+        {
+            stack = new Stack(5);
+            stack.Push(15);
+            stack.Push(10);
+            stack.Push(5);
+            stack.Push(1);
+        }
+
+        [TestMethod()]
+        public void PushTest()
+        {
+
+            object[] result = new object[5];
+            result[0] = 15;
+            result[1] = 10;
+            result[2] = 5;
+            result[3] = 1;
+            result[4] = 0;
+            stack.Push(0);
+
+            CollectionAssert.AreEqual(result, stack.Container);
+            Assert.AreEqual(result.Length, stack.Index);
+        }
+
+        [TestMethod()]
+        public void PopTest()
+        {
+
+            object result = 1;
+            object expected = stack.Pop();
+            Assert.AreEqual(result, expected);
+            Assert.AreEqual(3, stack.Index);
+            Assert.AreEqual(5, stack.Size);
+            Assert.AreEqual(0, stack.Container[stack.Index]);
+        }
+
+        [TestMethod()]
+        public void PeekTest()
+        {
+
+            object result = 1;
+            object expected = stack.Peek();
+            Assert.AreEqual(result, expected);
+            Assert.AreEqual(4, stack.Index);
+            Assert.AreEqual(5, stack.Size);
+            Assert.AreEqual(result, stack.Container[stack.Index - 1]);
+        }
+
+        [TestMethod()]
+        public void GetSizeTest()
+        {
+            int expectedSize = 5;
+            int expectedRemainingSize = 1;
+            Assert.AreEqual(expectedRemainingSize, stack.SizeRemaining());
+            Assert.AreEqual(expectedSize, stack.Size);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(StackOverflowException))]
+        public void PushToFullStackTest()
+        {
+            stack.Push(5);
+            stack.Push(7);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Stack.StackUnderflowException))]
+        public void PopFromEmptyStackTest()
+        {
+            while (true)
+            {
+                stack.Pop();
+            }
+        }
+
+    }
+}

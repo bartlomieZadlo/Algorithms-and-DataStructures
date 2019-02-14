f40540be723956591bffed5752345f0ad50db468
unknown <bartlomiejzadlo@gmail.com>
2019-02-14 12:56:28 +0100

    Basic implementation of HashMap without resizing. Added basic tests;

---
+++ AlgoDs/AlgoDs/DataStructures/HashMap.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDs.DataStructures
+{
+    public class HashMap
+    {
+        private List<object[]> container;
+        private int hashIndex;
+        private int hashedKey;
+
+        public HashMap(int size)
+        {
+            Container = new List<object[]>(size);
+            PopulateContainer(size);
+        }
+
+        private void PopulateContainer(int size)
+        {
+
+            for (int externalIndex = 0; externalIndex < size; externalIndex++)
+            {
+                Container.Add(new object[2 * size]);
+                PopulateInternalArrays(externalIndex);   
+            }
+        }
+
+        private void PopulateInternalArrays(int externalIndex)
+        {
+            for (int internalIndex = 0; internalIndex < Container[externalIndex].Length; internalIndex++)
+            {
+                Container[externalIndex][internalIndex] = null;
+            }
+        }
+
+        private void HashKey(object key)
+        {
+            HashIndex = Math.Abs(key.GetHashCode() % Container.Capacity - 1);
+            HashedKey = Math.Abs(key.GetHashCode() % 2 * Container.Capacity - 1);
+        }
+
+
+        public List<object[]> Container { get => container; set => container = value; }
+        public int HashedKey { get => hashedKey; set => hashedKey = value; }
+        public int HashIndex { get => hashIndex; set => hashIndex = value; }
+
+        public void Add(object key, object value)
+        {
+
+
+            HashKey(key);
+            
+            Container[HashIndex][HashedKey] = value;
+        }
+
+        public object GetValue(object key)
+        {
+            HashKey(key);
+
+            return Container[HashIndex][HashedKey];
+        }
+
+        public bool Remove(object key)
+        {
+            HashKey(key);
+
+            foreach (object value in Container[HashIndex])
+            {
+                if (Container[HashIndex][HashedKey] != null)
+                {
+                    Container[HashIndex][HashedKey] = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ClearAll()
+        {
+            int baseSize = Container.Count;
+            Container = new List<object[]>(baseSize);
+            PopulateContainer(baseSize);
+        }
+    
+    }
+}
---
+++ AlgoDs/AlgoDsTests/DataStructures/HashMapTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgoDs.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDs.DataStructures.Tests
+{
+    [TestClass()]
+    public class HashMapTests
+    {
+        private HashMap hashMap;
+
+        [TestInitialize()]
+        public void TestInit()
+        {
+            hashMap = new HashMap(500);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            string[] expectedKeys = { "test1", "test2", "test3" };
+            string[] expectedValues = { "test3", "test4", "test5" };
+            hashMap.Add("test1", "test3");
+            hashMap.Add("test2", "test4");
+            hashMap.Add("test3", "test5");
+
+            for(int i = 0; i < expectedKeys.Length; i++)
+            {
+                Assert.AreEqual(expectedValues[i], hashMap.GetValue(expectedKeys[i]));
+            }
+        }
+
+        [TestMethod()]
+        public void GetNonexistentElementTest()
+        {
+            hashMap.Add("test1", "test3");
+            hashMap.Add("test2", "test4");
+            hashMap.Add("test3", "test5");
+
+            Assert.AreEqual(null, hashMap.GetValue("test4"));   
+        }
+
+        [TestMethod()]
+        public void RemoveElementTest()
+        {
+            hashMap.Add("test1", "test3");
+            hashMap.Add("test2", "test4");
+            hashMap.Add("test3", "test5");
+            hashMap.Add("test4", "test5");
+
+
+            Assert.IsTrue(hashMap.Remove("test4"));
+            Assert.AreEqual(null, hashMap.GetValue("test4"));
+        }
+
+        [TestMethod()]
+        public void ClearAllTest()
+        {
+            string[] expectedKeys = { "test1", "test2", "test3" };
+            
+
+            hashMap.Add("test1", "test3");
+            hashMap.Add("test2", "test4");
+            hashMap.Add("test3", "test5");
+
+            hashMap.ClearAll();
+
+            for (int i = 0; i < expectedKeys.Length; i++)
+            {
+                Assert.AreEqual(null, hashMap.GetValue(expectedKeys[i]));
+            }
+        }
+
+
+    }
+}
